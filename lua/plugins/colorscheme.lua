11df126cd8e987007b21a35df41116846fb093e7
Aurelien Campergue <aurelien.campergue@gmail.com>
2023-02-10 22:19:25 +0100

    include and test colorschemes, update few modules

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  { "tomasiser/vim-code-dark" },
+
+  -- Configure LazyVim to load tokyonight-night
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "tokyonight-night",
+      -- colorscheme = "torte",
+      -- colorscheme = "default",
+      colorscheme = "codedark",
+    },
+  },
+}
